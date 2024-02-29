a9aea66e0ee6a1d75ec056704250cf74f419fb13
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-02-29 23:02:50 +0200

    add task

--- Working with Abstraction/WorkingWithAbstraction/src/cards1/CardSuits.java
+++ Working with Abstraction/WorkingWithAbstraction/src/cards1/CardSuits.java
@@ -1,4 +1,4 @@
-package cards;
+package cards1;
 
 public enum CardSuits {
     CLUBS,
---
+++ Working with Abstraction/WorkingWithAbstraction/src/cards1/Main.java
@@ -0,0 +1,11 @@
+package cards1;
+
+public class Main {
+    public static void main(String[] args) {
+        CardSuits[] cardSuits = CardSuits.values();
+        System.out.println("Card Suits:");
+        for (CardSuits card : cardSuits) {
+            System.out.printf("Ordinal value: %d; Name value: %s%n", card.ordinal(), card.name());
+        }
+    }
+}
