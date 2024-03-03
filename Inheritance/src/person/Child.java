380502116a69d05a811b8b976c9bb1bd89c35390
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-03-03 22:37:39 +0200

    Update Child.java

--- Inheritance/src/person/Child.java
+++ Inheritance/src/person/Child.java
@@ -6,14 +6,5 @@ public class Child extends Person{
     public Child(String name, int age) {
         super(name, age);
     }
-
-    @Override
-    public int getAge() {
-        return super.getAge();
-    }
-
-    @Override
-    public String getName() {
-        return super.getName();
-    }
+    
 }
