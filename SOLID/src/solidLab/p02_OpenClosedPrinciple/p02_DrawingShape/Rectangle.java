ec66109232b31921a7ded1dfadb14fbdd0c3852a
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-03-14 23:30:39 +0200

    Update Rectangle.java

--- SOLID/src/solidLab/p02_OpenClosedPrinciple/p02_DrawingShape/Rectangle.java
+++ SOLID/src/solidLab/p02_OpenClosedPrinciple/p02_DrawingShape/Rectangle.java
@@ -4,4 +4,8 @@ import solidLab.p02_OpenClosedPrinciple.p02_DrawingShape.interfaces.Shape;
 
 public class Rectangle implements Shape {
 
+    @Override
+    public double getArea() {
+        return 3.14;
+    }
 }
