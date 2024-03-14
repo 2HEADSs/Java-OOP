58f7e8204e9131555f971a70003576f3381041b4
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-03-14 23:30:41 +0200

    Create Renderer.java

---
+++ SOLID/src/solidLab/p02_OpenClosedPrinciple/p02_DrawingShape/Renderer.java
@@ -0,0 +1,9 @@
+package solidLab.p02_OpenClosedPrinciple.p02_DrawingShape;
+
+import solidLab.p02_OpenClosedPrinciple.p02_DrawingShape.interfaces.Shape;
+
+public class Renderer {
+    public void draw(Shape shape){
+        shape.getArea();
+    }
+}
