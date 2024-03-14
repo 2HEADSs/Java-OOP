05da6633f8b98ef0b1b283b1c7b51621f597ed77
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-03-14 23:30:35 +0200

    Update DrawingManagerImpl.java

--- SOLID/src/solidLab/p02_OpenClosedPrinciple/p02_DrawingShape/DrawingManagerImpl.java
+++ SOLID/src/solidLab/p02_OpenClosedPrinciple/p02_DrawingShape/DrawingManagerImpl.java
@@ -9,22 +9,6 @@ public class DrawingManagerImpl implements DrawingManager {
 
     @Override
     public void draw(Shape shape) {
-        if (shape instanceof Circle)
-        {
-            this.drawCircle((Circle)shape);
-        }
-            else if (shape instanceof Rectangle)
-        {
-            this.drawRectangle(((Rectangle)shape));
-        }
-    }
-    public void drawCircle(Circle circle)
-    {
-        // Draw Circle
-    }
 
-    public void drawRectangle(Rectangle rectangle)
-    {
-        // Draw Rectangle
     }
 }
