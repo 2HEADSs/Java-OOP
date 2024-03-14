1581509eb42a00f57939ae1a7ea151171906805b
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-03-14 23:30:33 +0200

    Update Circle.java

--- SOLID/src/solidLab/p02_OpenClosedPrinciple/p02_DrawingShape/Circle.java
+++ SOLID/src/solidLab/p02_OpenClosedPrinciple/p02_DrawingShape/Circle.java
@@ -3,4 +3,8 @@ package solidLab.p02_OpenClosedPrinciple.p02_DrawingShape;
 import solidLab.p02_OpenClosedPrinciple.p02_DrawingShape.interfaces.Shape;
 
 public class Circle implements Shape {
+    @Override
+    public double getArea() {
+        return Math.PI + 2;
+    }
 }
