4b58ace3d5d5ce7e62bc1cd749364414e96d20c8
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-03-15 22:50:50 +0200

    Update Chocolate.java

--- SOLID/src/solid/products/Chocolate.java
+++ SOLID/src/solid/products/Chocolate.java
@@ -1,6 +1,6 @@
 package solid.products;
 
-public class Chocolate {
+public class Chocolate implements Product {
 
     public static final double CALORIES_PER_100_GRAMS = 575.0;
 
@@ -12,5 +12,10 @@ public class Chocolate {
 
     public double getGrams() {
         return grams;
+    }
+
+    @Override
+    public double amountOfCalories() {
+        return CALORIES_PER_100_GRAMS / 100 * getGrams();
     }
 }
