cffdabaf47e355aa22d173b116d1adc176f06d4e
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-03-16 10:52:15 +0200

    Update Main.java

--- SOLID/src/solid/Main.java
+++ SOLID/src/solid/Main.java
@@ -1,8 +1,14 @@
 package solid;
 
+import solid.products.Cloud;
+import solid.products.QuantityCalculator;
+
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        Cloud cloud = new Cloud();
+        QuantityCalculator.average(List.of(cloud));
     }
 }
