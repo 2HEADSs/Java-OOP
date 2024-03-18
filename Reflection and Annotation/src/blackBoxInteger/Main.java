22a9ce658ec62e055da272afca655f174753b085
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-03-18 11:39:07 +0200

    add task

--- Reflection and Annotation/src/blackBoxInteger/Main.java
+++ Reflection and Annotation/src/blackBoxInteger/Main.java
@@ -1,7 +1,67 @@
 package blackBoxInteger;
 
-public class Main {
-    public static void main(String[] args) {
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
+public class Main {
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchFieldException {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        Class<BlackBoxInt> clazz = BlackBoxInt.class;
+
+        Constructor<BlackBoxInt> constructor = clazz.getDeclaredConstructor();
+        constructor.setAccessible(true);
+        BlackBoxInt blackBoxInt = constructor.newInstance();
+        Field innerValue = clazz.getDeclaredField("innerValue");
+
+        List<Method> methods = Arrays.stream(clazz.getDeclaredMethods()).collect(Collectors.toList());
+
+
+        while (!input.equals("END")) {
+            String command = input.split("_")[0];
+            int num = Integer.parseInt(input.split("_")[1]);
+            switch (command) {
+                case "add":
+                    executeCommand( blackBoxInt, innerValue, num, "add", methods);
+                    break;
+                case "subtract":
+                    executeCommand( blackBoxInt, innerValue, num, "subtract", methods);
+                    break;
+                case "multiply":
+                    executeCommand( blackBoxInt, innerValue, num, "multiply", methods);
+                    break;
+                case "divide":
+                    executeCommand( blackBoxInt, innerValue, num, "divide", methods);
+                    break;
+                case "leftShift":
+                    executeCommand( blackBoxInt, innerValue, num, "leftShift", methods);
+                    break;
+                case "rightShift":
+                    executeCommand( blackBoxInt, innerValue, num, "rightShift", methods);
+                    break;
+            }
+
+            input = scanner.nextLine();
+        }
+    }
+
+    private static void executeCommand(BlackBoxInt blackBoxInt, Field innerValue, int num, String command, List<Method> methods) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        Method currentMethod = null;
+        for (Method method : methods) {
+            if (method.getName().equals(command)) {
+                currentMethod = method;
+                break;
+            }
+        }
+        currentMethod.setAccessible(true);
+        currentMethod.invoke(blackBoxInt, num);
+        innerValue.setAccessible(true);
+        System.out.println(innerValue.get(blackBoxInt));
     }
 }
--- Reflection and Annotation/src/harvestingFields/Main.java
+++ Reflection and Annotation/src/harvestingFields/Main.java
@@ -4,65 +4,66 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.util.*;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Map<String, List<Field>> fieldsMap = getFieldsMap();
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		Map<String, List<Field>> fieldsMap = getFieldsMap();
 
-        Consumer<Field> printerFiled = field -> {
-            System.out.println(Modifier.toString(field.getModifiers())
-                    + " "
-                    + field.getType().getSimpleName()
-                    + " "
-                    + field.getName());
-        };
+		Consumer<Field> printerFiled = field -> {
+			System.out.println(Modifier.toString(field.getModifiers())
+					+ " "
+					+ field.getType().getSimpleName()
+					+ " "
+					+ field.getName());
+		};
 
-        String command = scanner.nextLine();
-        while (!command.equals("HARVEST")) {
-            switch (command) {
-                case "private":
-                    fieldsMap.get("private").forEach(printerFiled);
-                    break;
-                case "public":
-                    fieldsMap.get("public").forEach(printerFiled);
-                    break;
-                case "protected":
-                    fieldsMap.get("protected").forEach(printerFiled);
-                    break;
-                case "all":
-                    fieldsMap.get("all").forEach(printerFiled);
-                    break;
-            }
+		String command = scanner.nextLine();
+		while (!command.equals("HARVEST")) {
+			switch (command) {
+				case "private":
+					fieldsMap.get("private").forEach(printerFiled);
+					break;
+				case "public":
+					fieldsMap.get("public").forEach(printerFiled);
+					break;
+				case "protected":
+					fieldsMap.get("protected").forEach(printerFiled);
+					break;
+				case "all":
+					fieldsMap.get("all").forEach(printerFiled);
+					break;
+			}
 
-            command = scanner.nextLine();
-        }
+			command = scanner.nextLine();
+		}
 
 
-    }
+	}
 
-    private static Map<String, List<Field>> getFieldsMap() {
-        Map<String, List<Field>> map = new HashMap<>();
-        map.put("private", new ArrayList<>());
-        map.put("public", new ArrayList<>());
-        map.put("protected", new ArrayList<>());
-        List<Field> allFields = Arrays.stream(RichSoilLand.class.getDeclaredFields()).toList();
-        map.put("all", allFields);
+	private static Map<String, List<Field>> getFieldsMap() {
+		Map<String, List<Field>> map = new HashMap<>();
+		map.put("private", new ArrayList<>());
+		map.put("public", new ArrayList<>());
+		map.put("protected", new ArrayList<>());
+		List<Field> allFields = Arrays.stream(RichSoilLand.class.getDeclaredFields()).collect(Collectors.toList());
+		map.put("all", allFields);
 
-        allFields.forEach(field -> {
-            String modifier = Modifier.toString(field.getModifiers());
-            switch (modifier) {
-                case "private":
-                    map.get("private").add(field);
-                    break;
-                case "public":
-                    map.get("public").add(field);
-                    break;
-                case "protected":
-                    map.get("protected").add(field);
-                    break;
-            }
-        });
-        return map;
-    }
+		allFields.forEach(field -> {
+			String modifier = Modifier.toString(field.getModifiers());
+			switch (modifier) {
+				case "private":
+					map.get("private").add(field);
+					break;
+				case "public":
+					map.get("public").add(field);
+					break;
+				case "protected":
+					map.get("protected").add(field);
+					break;
+			}
+		});
+		return map;
+	}
 }
