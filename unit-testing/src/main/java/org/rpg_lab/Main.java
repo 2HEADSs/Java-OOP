f37c15f96ee6b10e709b7d93886b733c40f35a43
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-03-19 23:19:46 +0200

    Update Main.java

--- unit-testing/src/main/java/org/rpg_lab/Main.java
+++ unit-testing/src/main/java/org/rpg_lab/Main.java
@@ -3,6 +3,7 @@ package org.rpg_lab;
 public class Main {
     public static void main(String[] args) {
     Axe axe = new Axe(13, 42);
-
+        System.out.println(axe.getAttackPoints());
+        System.out.println(axe.getDurabilityPoints());
     }
 }
