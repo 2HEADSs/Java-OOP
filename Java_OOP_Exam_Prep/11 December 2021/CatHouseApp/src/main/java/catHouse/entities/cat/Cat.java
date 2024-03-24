092940a329378af285cce7a5f7f458ecdbe30f28
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-03-24 11:56:18 +0200

    Update Cat.java

--- Java_OOP_Exam_Prep/11 December 2021/CatHouseApp/src/main/java/catHouse/entities/cat/Cat.java
+++ Java_OOP_Exam_Prep/11 December 2021/CatHouseApp/src/main/java/catHouse/entities/cat/Cat.java
@@ -2,12 +2,8 @@ package catHouse.entities.cat;
 
 public interface Cat {
     String getName();
-
     void setName(String name);
-
     int getKilograms();
-
     double getPrice();
-
     void eating();
 }
