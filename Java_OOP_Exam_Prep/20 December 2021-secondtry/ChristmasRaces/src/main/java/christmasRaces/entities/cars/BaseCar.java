7a09a2e186adbade7689b6fbd7956e881574c7ee
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-03-27 23:02:40 +0200

    Create BaseCar.java

---
+++ Java_OOP_Exam_Prep/20 December 2021-secondtry/ChristmasRaces/src/main/java/christmasRaces/entities/cars/BaseCar.java
@@ -0,0 +1,55 @@
+package christmasRaces.entities.cars;
+
+import christmasRaces.common.ExceptionMessages;
+
+public abstract class BaseCar implements Car{
+    private String model;
+    private int horsePower;
+    private double cubicCentimeters;
+
+    public BaseCar(String model, int horsePower, double cubicCentimeters) {
+        this.setModel(model);
+        this.setHorsePower(horsePower);
+        this.setCubicCentimeters(cubicCentimeters);
+    }
+
+    @Override
+    public String getModel() {
+        return null;
+    }
+
+    public void setModel(String model) {
+        if(model ==null ||model.trim().length() <4){
+            throw new IllegalArgumentException(
+                    String.format(ExceptionMessages.INVALID_MODEL, model,4)
+            );
+        }
+        this.model = model;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.checkHorsePower(horsePower);
+        this.horsePower = horsePower;
+    }
+    protected abstract void checkHorsePower(int horsePower);
+
+
+    public void setCubicCentimeters(double cubicCentimeters) {
+        this.cubicCentimeters = cubicCentimeters;
+    }
+
+    @Override
+    public int getHorsePower() {
+        return this.horsePower;
+    }
+
+    @Override
+    public double getCubicCentimeters() {
+        return this.cubicCentimeters;
+    }
+
+    @Override
+    public double calculateRacePoints(int laps) {
+        return this.cubicCentimeters/this.horsePower * laps;
+    }
+}
