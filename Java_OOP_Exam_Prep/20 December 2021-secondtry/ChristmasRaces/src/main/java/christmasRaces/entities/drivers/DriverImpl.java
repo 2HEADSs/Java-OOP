186b2f2511fa2593a255b11e2bab877f4662eecf
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-03-27 23:03:50 +0200

    Create DriverImpl.java

---
+++ Java_OOP_Exam_Prep/20 December 2021-secondtry/ChristmasRaces/src/main/java/christmasRaces/entities/drivers/DriverImpl.java
@@ -0,0 +1,35 @@
+package christmasRaces.entities.drivers;
+
+import christmasRaces.entities.cars.Car;
+
+public class DriverImpl implements Driver{
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public Car getCar() {
+        return null;
+    }
+
+    @Override
+    public int getNumberOfWins() {
+        return 0;
+    }
+
+    @Override
+    public void addCar(Car car) {
+
+    }
+
+    @Override
+    public void winRace() {
+
+    }
+
+    @Override
+    public boolean getCanParticipate() {
+        return false;
+    }
+}
