ea337d6df5c68cc463510a7b739845c60d1abf03
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-03-28 19:52:41 +0200

    Create RaceImpl.java

---
+++ Java_OOP_Exam_Prep/20 December 2021-secondtry/ChristmasRaces/src/main/java/christmasRaces/entities/races/RaceImpl.java
@@ -0,0 +1,79 @@
+package christmasRaces.entities.races;
+
+import christmasRaces.common.ExceptionMessages;
+import christmasRaces.entities.drivers.Driver;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class RaceImpl implements Race {
+
+    private String name;
+    private int laps;
+    private Collection<Driver> drivers;
+
+    public RaceImpl(String name, int laps) {
+        this.setName(name);
+        this.setLaps(laps);
+        this.setDrivers();
+    }
+
+    public void setName(String name) {
+        if (name == null || name.trim().length() < 5) {
+            throw new IllegalArgumentException(
+                    String.format(ExceptionMessages.INVALID_NAME, name, 5)
+            );
+        }
+        this.name = name;
+    }
+
+    public void setDrivers() {
+        this.drivers = new ArrayList<>();
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    public void setLaps(int laps) {
+        if (laps < 1) {
+            throw new IllegalArgumentException(
+                    String.format(ExceptionMessages.INVALID_NUMBER_OF_LAPS, 1)
+            );
+        }
+        this.laps = laps;
+    }
+
+    @Override
+    public int getLaps() {
+        return 0;
+    }
+
+    @Override
+    public Collection<Driver> getDrivers() {
+        return null;
+    }
+
+
+
+    @Override
+    public void addDriver(Driver driver) {
+        if (driver == null) {
+            throw new IllegalArgumentException(
+                    String.format(ExceptionMessages.DRIVER_INVALID)
+            );
+        } else if (!driver.getCanParticipate()) {
+            throw new IllegalArgumentException(
+                    String.format(ExceptionMessages.DRIVER_NOT_PARTICIPATE,driver.getName())
+            );
+        }else if(drivers.contains(driver)){
+            throw new IllegalArgumentException(
+                    String.format(ExceptionMessages.DRIVER_ALREADY_ADDED, driver.getName(),this.name)
+            );
+        }
+
+        drivers.add(driver);
+    }
+}
