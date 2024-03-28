cabb3f9f68e21ba65ec2983d4aa68cd9808ea0c4
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-03-28 23:48:48 +0200

    Create Mouse.java

---
+++ Java_OOP_Exam_Prep/11 December 2021 - secondtry/CatHouseApp/src/main/java/catHouse/entities/toys/Mouse.java
@@ -0,0 +1,9 @@
+package catHouse.entities.toys;
+
+public class Mouse extends BaseToy{
+    private static final int MOUSE_SOFTENSS = 5;
+    private static final int MOUSE_PRICE = 15;
+    public Mouse() {
+        super(MOUSE_SOFTENSS, MOUSE_PRICE);
+    }
+}
