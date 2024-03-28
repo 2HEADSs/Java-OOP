ee44525ab216aecec4e8b1cb37d906ff8de14193
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-03-28 23:48:42 +0200

    Create BaseCat.java

---
+++ Java_OOP_Exam_Prep/11 December 2021 - secondtry/CatHouseApp/src/main/java/catHouse/entities/cat/BaseCat.java
@@ -0,0 +1,29 @@
+package catHouse.entities.cat;
+
+public abstract class BaseCat implements Cat{
+    
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public void setName(String name) {
+
+    }
+
+    @Override
+    public int getKilograms() {
+        return 0;
+    }
+
+    @Override
+    public double getPrice() {
+        return 0;
+    }
+
+    @Override
+    public void eating() {
+
+    }
+}
