2c73180b630e6a60dddb25c6e332f2c98c5e43ab
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-03-28 23:48:44 +0200

    Create Ball.java

---
+++ Java_OOP_Exam_Prep/11 December 2021 - secondtry/CatHouseApp/src/main/java/catHouse/entities/toys/Ball.java
@@ -0,0 +1,9 @@
+package catHouse.entities.toys;
+
+public class Ball extends BaseToy{
+    private static final int BALL_SOFTENSS = 1;
+    private static final int BALL_PRICE = 1;
+    public Ball() {
+        super(BALL_SOFTENSS, BALL_PRICE);
+    }
+}
