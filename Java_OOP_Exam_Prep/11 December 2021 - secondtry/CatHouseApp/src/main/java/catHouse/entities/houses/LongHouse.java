54ffa04dbbdbec74a4c8bdc2ad8e59d29e4a3848
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-03-29 20:10:23 +0200

    Create LongHouse.java

---
+++ Java_OOP_Exam_Prep/11 December 2021 - secondtry/CatHouseApp/src/main/java/catHouse/entities/houses/LongHouse.java
@@ -0,0 +1,8 @@
+package catHouse.entities.houses;
+
+public class LongHouse extends BaseHouse{
+    private static final int LONGHOUSE_CAPACITY = 15;
+    public LongHouse(String name) {
+        super(name, LONGHOUSE_CAPACITY);
+    }
+}
