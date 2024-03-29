5d73d7fdd09186cf196ca1384cc2044caee6216e
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-03-29 20:10:20 +0200

    Create BaseHouse.java

---
+++ Java_OOP_Exam_Prep/11 December 2021 - secondtry/CatHouseApp/src/main/java/catHouse/entities/houses/BaseHouse.java
@@ -0,0 +1,95 @@
+package catHouse.entities.houses;
+
+import catHouse.common.ConstantMessages;
+import catHouse.common.ExceptionMessages;
+import catHouse.entities.cat.Cat;
+import catHouse.entities.toys.Toy;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+public class BaseHouse implements House {
+    private String name;
+    private int capacity;
+    private Collection<Toy> toys;
+    private Collection<Cat> cats;
+
+    public BaseHouse(String name, int capacity) {
+        this.setName(name);
+        this.capacity = capacity;
+    }
+
+    @Override
+    public int sumSoftness() {
+        int sum = 0;
+        for (Toy toy : toys) {
+            sum += toy.getSoftness();
+        }
+        return sum;
+    }
+
+    @Override
+    public void addCat(Cat cat) {
+        if (this.getCats().size() >= this.capacity) {
+            throw new IllegalStateException(String.format(ConstantMessages.NOT_ENOUGH_CAPACITY_FOR_CAT));
+        }
+        this.cats.add(cat);
+    }
+
+    @Override
+    public void removeCat(Cat cat) {
+        this.cats.remove(cat);
+    }
+
+    @Override
+    public void buyToy(Toy toy) {
+        this.toys.add(toy);
+    }
+
+    @Override
+    public void feeding() {
+        for (Cat cat : this.cats) {
+            cat.eating();
+        }
+    }
+
+    @Override
+    public String getStatistics() {
+        StringBuilder sb = new StringBuilder();
+        if(this.cats.isEmpty()){
+            sb.append("none").append(System.lineSeparator());
+        } else {
+            sb.append(this.name+ " " + this.getClass().getSimpleName());
+            sb.append(this.getCats().stream()
+                    .map(Cat::getName)
+                    .collect(Collectors.joining(" ")).trim()
+            );
+        }
+        sb.append(System.lineSeparator());
+        sb.append("Toys: %d Softenss: %d",this.getToys().size(), this.sumSoftness());
+        return sb.toString().trim();
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public void setName(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new NullPointerException(ExceptionMessages.HOUSE_NAME_CANNOT_BE_NULL_OR_EMPTY);
+        }
+        this.name = name;
+    }
+
+    @Override
+    public Collection<Cat> getCats() {
+        return this.cats;
+    }
+
+    @Override
+    public Collection<Toy> getToys() {
+        return this.toys;
+    }
+}
