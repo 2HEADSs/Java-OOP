646d23d9e7298c3e8d10681418a6a1292740de77
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-04-02 22:11:10 +0300

    Create ClimberRepository.java

---
+++ Java_OOP_Exam_Prep/19 December 2023/ClimbersApp/src/main/java/climbers/repositories/ClimberRepository.java
@@ -0,0 +1,41 @@
+package climbers.repositories;
+
+import climbers.models.climber.Climber;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+
+public class ClimberRepository implements Repository<Climber> {
+    private Map<String, Climber> climbers;
+
+    @Override
+    public Collection<Climber> getCollection() {
+        return Collections.unmodifiableCollection(climbers.values());
+    }
+
+    @Override
+    public void add(Climber climber) {
+        this.climbers.put(climber.getName(), climber);
+    }
+
+    @Override
+    public boolean remove(Climber climber) {
+        if (this.climbers.containsKey(climber.getName())) {
+            this.climbers.remove(climber.getName());
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Climber byName(String name) {
+        if (climbers.containsKey(name)) {
+            return this.climbers.get(name);
+        } else {
+            return null;
+        }
+
+    }
+
+}
