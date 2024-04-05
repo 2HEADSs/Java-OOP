cf7c7e81ab163774754d7136903fc779e08a957a
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-04-05 23:44:15 +0300

    Create MagicRepositoryImpl.java

---
+++ Java_OOP_Exam_Prep/19 December 2022/MagicGameApp/src/main/java/magicGame/repositories/MagicRepositoryImpl.java
@@ -0,0 +1,49 @@
+package magicGame.repositories;
+
+import magicGame.common.ExceptionMessages;
+import magicGame.models.magics.Magic;
+import magicGame.repositories.interfaces.MagicRepository;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class MagicRepositoryImpl implements MagicRepository {
+
+    private Collection<Magic> data;
+
+    public MagicRepositoryImpl() {
+        this.data = new ArrayList<>();
+    }
+
+
+    @Override
+    public Collection getData() {
+        return this.data;
+    }
+
+    @Override
+    public void addMagic(Magic magic) {
+        if(magic == null){
+            throw new NullPointerException(ExceptionMessages.INVALID_MAGIC_REPOSITORY);
+        }
+        this.data.add(magic);
+    }
+
+    @Override
+    public boolean removeMagic(Magic magic) {
+        return this.data.remove(magic);
+
+    }
+
+    @Override
+    public Object findByName(String name) {
+        for (Magic magic : this.data) {
+                if(magic.getName().equals(name)){
+                    return magic;
+                }
+        }
+        return null;
+    }
+}
