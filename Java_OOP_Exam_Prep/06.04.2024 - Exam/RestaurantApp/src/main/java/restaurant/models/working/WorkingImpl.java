68b8d55e95408fe884bc6db5b5c11dc5df935e3f
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-04-07 22:12:07 +0300

    Create WorkingImpl.java

---
+++ Java_OOP_Exam_Prep/06.04.2024 - Exam/RestaurantApp/src/main/java/restaurant/models/working/WorkingImpl.java
@@ -0,0 +1,21 @@
+package restaurant.models.working;
+
+import restaurant.models.client.Client;
+import restaurant.models.waiter.Waiter;
+
+import java.util.Collection;
+
+public class WorkingImpl implements Working {
+    @Override
+    public void takingOrders(Client client, Collection<Waiter> waiters) {
+        Collection<String> orders = client.getClientOrders();
+        for (Waiter waiter : waiters) {
+            while (waiter.canWork() && orders.iterator().hasNext()) {
+                String currentOrder = orders.iterator().next();
+                waiter.work();
+                waiter.takenOrders().getOrdersList().add(currentOrder);
+                orders.remove(currentOrder);
+            }
+        }
+    }
+}
